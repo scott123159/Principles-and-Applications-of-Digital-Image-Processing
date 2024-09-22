a512661f4fc16737c6e8cbb52bffd9aee415511e
You-Hua Liao <r11631041@ntu.edu.tw>
2024-09-22 14:17:13 +0800

    add open, exit, undo, redo UI components and functionality that display image on QLabel

--- HW2/mainwindow.cpp
+++ HW2/mainwindow.cpp
@@ -1,14 +1,51 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->undoAction->setDisabled(true);
+    ui->redoAction->setDisabled(true);
+    this->setWindowTitle("HW2");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_exitAction_triggered()
+{
+    this->close();
+}
+
+void MainWindow::on_openFileAction_triggered()
+{
+    /*Open image file that store in current folder*/
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Image"),
+                                                    ".\\",
+                                                    tr("Images (*.JPEG, *.BMP, *.PNG);;All Files (*.*)"));
+
+    /*If file is not selected, then return program*/
+    if (fileName.isNull()) return;
+    /*Set the program title with file that you selected*/
+    this->setWindowTitle(this->windowTitle() + " " + fileName.mid(fileName.lastIndexOf('/') + 1));
+    /*Set current state*/
+    this->image = QImage(fileName);
+    /*Display the image that you selected on the QLabel*/
+    ui->image->setPixmap(QPixmap::fromImage(image));
+}
+
+void MainWindow::on_undoAction_triggered()
+{
+    if (undo.isEmpty()) return;
+}
+
+void MainWindow::on_redoAction_triggered()
+{
+    if (redo.isEmpty()) return;
 }
--- HW2/mainwindow.h
+++ HW2/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStack>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +18,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_exitAction_triggered();
+
+    void on_openFileAction_triggered();
+
+    void on_undoAction_triggered();
+
+    void on_redoAction_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QStack<QImage> undo;
+    QStack<QImage> redo;
+    QImage image;
 };
 #endif // MAINWINDOW_H
